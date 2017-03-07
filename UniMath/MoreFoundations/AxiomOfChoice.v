5eeb14e002c555b51b9c42b1c057d0411f1129dc
Tomi Pannila <tpannila@gmail.com>
2017-03-07 13:43:22 +0200

    Faster unfold

--- UniMath/MoreFoundations/AxiomOfChoice.v
+++ UniMath/MoreFoundations/AxiomOfChoice.v
@@ -19,24 +19,33 @@ Definition AxiomOfChoice_surj : hProp := ∀ (X:hSet) (Y:UU) (f:Y→X), issurjec
 
 (** ** Implications between forms of the Axiom of Choice *)
 
+Lemma AC_impl2_issurj (X : hSet) (P : X -> UU) (ne : ∏ x : X, ∥ P x ∥)
+  (T := (∑ x, P x)%type) (f := pr1 : T -> X) :
+  issurjective f.
+Proof.
+  intros x.
+  use (hinhuniv _ (ne x)).
+  intros p.
+  use hinhpr.
+  use hfiberpair.
+  - exact (tpair _ x p).
+  - use idpath.
+Qed.
+
 Lemma AC_impl2 : AxiomOfChoice <-> AxiomOfChoice_surj.
 Proof.
   split.
   - intros AC X Y f surj.
-    apply (squash_to_prop (AC _ _ surj) (propproperty _)).
-    intro s. apply hinhpr.
-    exists (λ y, hfiberpr1 f y (s y)).
-    exact  (λ y, hfiberpr2 f y (s y)).
+    use (squash_to_prop (AC _ _ surj) (propproperty _)).
+    intro s.
+    use hinhpr.
+    use tpair.
+    + exact (λ y, hfiberpr1 f y (s y)).
+    + exact (λ y, hfiberpr2 f y (s y)).
   - intros AC X P ne.
-    set (T := (∑ x, P x)%type).
-    set (f := pr1 : T -> X).
-    assert (k : issurjective f).
-    { intros x. simple refine (hinhuniv _ (ne x)); intro p. apply hinhpr. exists (x,,p). reflexivity. }
-    simple refine (hinhuniv _ (AC X T f k)).
-    intro sec. induction sec as [g e]. apply hinhpr. intro x.
-    assert (e' := e x); simpl in e'; clear e.
-    induction (g x) as [x' p]; simpl in e'.
-    induction e'. exact p.
+    use (hinhuniv _ (AC X _ _ (AC_impl2_issurj X P ne))).
+    intros sec. use hinhpr. intros x.
+    induction (pr2 sec x). exact (pr2 (pr1 sec x)).
 Defined.
 
 (** ** The Axiom of Choice implies the Law of the Excluded Middle  *)
@@ -52,53 +61,65 @@ Defined.
     that the function [g ∘ f : bool -> bool] is constant iff P, and to observe that being constant is
     decidable, because equality in [bool] is decidable. *)
 
+Definition AC_to_LEM_hrel (AC : AxiomOfChoice) (P : hProp) (ifb := bool_rect (λ _:bool, hProp)) :
+  hrel bool := λ x y, ifb (ifb htrue P y) (ifb P htrue y) x.
+
+Lemma AC_to_LEM_hrel_iseqrel (AC : AxiomOfChoice) (P : hProp) (ifb := bool_rect (λ _:bool, hProp)) :
+  iseqrel (AC_to_LEM_hrel AC P).
+Proof.
+  use iseqrelconstr.
+  - intros x y z a b. induction x.
+    + induction y.
+      * induction z; exact b.
+      * induction z.
+        -- exact tt.
+        -- exact a.
+    + induction y.
+      * induction z.
+        -- exact a.
+        -- exact tt.
+      * induction z; exact b.
+  - intros x. induction x; exact tt.
+  - intros x y a. induction x; induction y; exact a.
+Qed.
+
+Lemma AC_to_LEM_coll (AC : AxiomOfChoice) (P : hProp) (ifb := bool_rect (λ _:bool, hProp))
+      (E := eqrelpair (AC_to_LEM_hrel AC P) (AC_to_LEM_hrel_iseqrel AC P))
+      (Y := setquotinset E) (f := (setquotpr E : bool → Y) : bool → Y)
+      (sec : ∑ g : Y → boolset, ∀ x : Y, f (g x) = x) :
+  P <-> (pr1 sec) (f true) = (pr1 sec) (f false).
+Proof.
+  split.
+  - intro p. use maponpaths. use iscompsetquotpr. exact p.
+  - intro q.
+    assert (r : f true = f false).
+    {
+      intermediate_path (f ((pr1 sec) (f true))).
+      + use pathsinv0. use (pr2 sec).
+      + intermediate_path (f ((pr1 sec) (f false))).
+        * use maponpaths. use q.
+        * use (pr2 sec).
+    }
+    change (E true false).
+    use (invmap (weqpathsinsetquot _ _ _)).
+    change (f true = f false).
+    exact r.
+Qed.
+
 Theorem AC_to_LEM : AxiomOfChoice -> LEM.
 Proof.
   intros AC P.
   set (ifb := bool_rect (λ _:bool, hProp)).
   set (R := λ x y, ifb (ifb htrue P y) (ifb P htrue y) x); simpl in R.
-  assert (e : iseqrel R).
-  { repeat split.
-    { intros x y z a b. induction x.
-      - induction y.
-        + induction z; exact b.
-        + induction z.
-          * exact tt.
-          * exact a.
-      - induction y.
-        + induction z.
-          * exact a.
-          * exact tt.
-        + induction z; exact b. }
-    { intros x. induction x; exact tt. }
-    { intros x y a. induction x; induction y; exact a. } }
-  set (E := R,,e : eqrel bool).
+  set (e := AC_to_LEM_hrel_iseqrel AC P).
+  set (E := eqrelpair (AC_to_LEM_hrel AC P) (AC_to_LEM_hrel_iseqrel AC P)).
   set (Y := setquotinset E).
-  set (f := setquotpr E : bool -> Y).
-  assert (q := pr1 AC_impl2 AC Y boolset f (issurjsetquotpr E)).
-  apply (squash_to_prop q).
-  { apply isapropdec. apply propproperty. }
-  clear q.
+  set (f := setquotpr E).
+  use (squash_to_prop (pr1 AC_impl2 AC Y boolset f (issurjsetquotpr E)) (propproperty _)).
   intro sec.
-  induction sec as [g h].
-  (* prove P iff [g ∘ f] is constant *)
-  assert (coll : P <-> g (f true) = g (f false)).
-  { split.
-    { intro p. apply maponpaths. apply iscompsetquotpr. exact p. }
-    { intro q.
-      assert (r : f true = f false).
-      { intermediate_path (f (g (f true))).
-        { apply pathsinv0, h. }
-        intermediate_path (f (g (f false))).
-        { apply maponpaths, q. }
-        { apply h. } }
-      change (E true false).
-      apply (invmap (weqpathsinsetquot _ _ _)).
-      change (f true = f false).
-      exact r. } }
   (* the equation is decidable, and thus P is decidable, by the lemma above *)
-  apply (logeq_dec (issymm_logeq _ _ coll)).
-  apply isdeceqbool.
+  use (logeq_dec (issymm_logeq _ _ (AC_to_LEM_coll AC P sec))).
+  use isdeceqbool.
 Defined.
 
 (* end of file *)
